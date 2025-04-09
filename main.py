350c71a1bd6e5cbae956bda9ebc81a4e372e01d9
mjani89 <mjani89@gmail.com>
2025-04-10 00:17:03 +0300

    Added Player

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    Player.containers = (updatable,drawable)
+    player = Player(x,y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        for sprite in drawable:
+            sprite.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
